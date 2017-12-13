5a6d613dd76682aab003e005cb49c13d22398437
Claudina Cortez <claudinacr@gmail.com>
2017-12-13 14:33:33 -0300

    Implementación de F O O D M A P

---
+++ assets/js/data.js
@@ -0,0 +1,38 @@
+var data = [{
+  name: 'El Frito',
+  categoria: 'mexicana',
+  descripcion: 'lorem ipsum',
+  addres: 'Av. Jacinto Lara',
+  tlf: '737328239832',
+  coords: { lat: 11.9861785, lng: -70.2026626 },
+  platos: [{ name: 'Tacos', image: '../images/mexican food/taco.png' }, { name: 'Burritos', image: '../images/mexican food/burrito.png' }, { name: 'Enchilada', image: '../images/mexican food/enchilada.png' }, { name: 'Nachos', image: '../images/mexican food/nachos.png' }, { name: 'Tortillas', image: '../images/mexican food/tortillas.png' }]
+
+
+}, {
+  name: 'El Pescado Azul',
+  categoria: 'mediterranea',
+  descripcion: 'lorem ipsum',
+  addres: 'Av. Jacinto Lara',
+  tlf: '737328239832',
+  coords: { lat: 12.0727477, lng: -70.0054023 },
+  platos: [{ name: 'fish', image: '../images/mediterranean food/fish.png' }, { name: 'Frittata', image: '../images/mediterranean food/frittata.png' }, { name: 'Lasagna', image: '../images/mediterranean food/lasagna.png' }, { name: 'Panna-Cotta', image: '../images/mediterranean food/panna-cotta.png' }, { name: 'Risotto', image: '../images/mediterranean food/risotto.png' }]
+
+}, {
+  name: 'Mega Hamburguesa',
+  categoria: 'americana',
+  descripcion: 'lorem ipsum',
+  addres: 'Av. Jacinto Lara',
+  tlf: '737328239832',
+  coords: { lat: 12.1847948, lng: -70.0205299 },
+  platos: [{ name: 'BBQ', image: '../images/american food/bbq.png' }, { name: 'Cake', image: '../images/american food/cake.png' }, { name: 'Fast-Food', image: '../images/american food/fast-food.png' }, { name: 'French-Fries', image: '../images/american food/french-fries.png' }, { name: 'Hot Dogs', image: '../images/american food/hot-dogs.png' }]
+
+
+}, {
+  name: 'La Media Naranja',
+  categoria: 'italiana',
+  descripcion: 'lorem ipsum',
+  addres: 'Av. Jacinto Lara',
+  tlf: '737328239832',
+  coords: { lat: 12.1201766, lng: -69.9099454 },
+  platos: [{ name: 'Pasta', image: '../images/italian food/pasta.png' }, { name: 'Pizza', image: '../images/italian food/pizza.png' }, { name: 'Ravioli', image: '../images/italian food/ravioli.png' }, { name: 'Spaghetti', image: '../images/italian food/spaghetti.png' }, { name: 'Croissant', image: '../images/italian food/croissant.png' }]
+}]
---
+++ assets/js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+
+});
+var mapa;
+var opciones = {
+  center: { lat: 11.9108084, lng: -70.0299834 },
+  zoom: 10
+};
+
+
+
+function initMap() {
+  mapa = new google.maps.Map(document.getElementById('map'), opciones);
+
+  var restaurantes = [];
+  for (let i = 0; i < data.length; i++) {
+    const restaurante = data[i];
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(restaurante.coords),
+      title: restaurante.name,
+      map: mapa,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        fillColor: 'magenta',
+        fillOpacity: 0.8,
+      }
+    });
+    marker.setMap(mapa);
+    restaurantes.push(marker);
+
+  }
+
+}
+
+
+function setMarkers() {
+
+  for (let index = 0; index < restaurantes.length; index++) {
+    const restaurante = restaurantes[index];
+
+
+  }
+
+}
+
+
+// function clearMarkers() {
+//   for (var i = 0; i < markers.length; i++) {
+
+//     markers[i].setMap(null);
+//   }
+//   //markers = [];//use this to clear out all the markers.
+// }
+
+$(document).ready(function () {
+
+});
+var mapa;
+var opciones = {
+  center: { lat: 11.9108084, lng: -70.0299834 },
+  zoom: 10
+};
+
+
+$(document).ready(function () {
+  $('.fa-search').click(function () {
+    var categoria = $('#filtro-categoria').val();
+    for (var j = 0; j < data.length; j++) {
+      if (categoria === data[j].categoria) {
+
+        mapa = new google.maps.Map(document.getElementById('map'), opciones);
+        const restaurante = data[j];
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(data[j].coords),
+          title: restaurante.categoria,
+          map: mapa,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 10,
+            fillColor: 'magenta',
+            fillOpacity: 0.8,
+          }
+        });
+      }
+
+    }
+  });
+});
+
+
+
+
+

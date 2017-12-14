5371a098e4b25b413ca15e75994ca5cbe5a89c55
Claudina Cortez <claudinacr@gmail.com>
2017-12-14 09:29:43 -0300

    Implementacion de Food Map

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,17 +2,20 @@ var data = [{
   name: 'El Frito',
   categoria: 'mexicana',
   descripcion: 'lorem ipsum',
-  addres: 'Av. Jacinto Lara',
+  Precio: '1.000.000 Pesos',
+  address: 'Av. Jacinto Lara',
   tlf: '737328239832',
   coords: { lat: 11.9861785, lng: -70.2026626 },
-  platos: [{ name: 'Tacos', image: 'assets/images/mexican food/taco.png' }, { name: 'Burritos', image: 'assets/images/mexican food/burrito.png' }, { name: 'Enchilada', image: 'assets/images/mexican food/enchilada.png' }, { name: 'Nachos', image: 'assets/images/mexican food/nachos.png' }, { name: 'Tortillas', image: 'assets/images/mexican food/tortillas.png' }]
+  platos: [{ name: 'Tacos', image: 'assets/images/mexican food/taco.png', description: 'Comida muy tipica' }, { name: 'Burritos', image: 'assets/images/mexican food/burrito.png' }, { name: 'Enchilada', image: 'assets/images/mexican food/enchilada.png' }, { name: 'Nachos', image: 'assets/images/mexican food/nachos.png' }, { name: 'Tortillas', image: 'assets/images/mexican food/tortillas.png' }]
 
 
 }, {
   name: 'El Pescado Azul',
   categoria: 'mediterranea',
   descripcion: 'lorem ipsum',
-  addres: 'Av. Jacinto Lara',
+  Precio: '1.000.000 Pesos',
+
+  address: 'Av. Jacinto Lara',
   tlf: '737328239832',
   coords: { lat: 12.0727477, lng: -70.0054023 },
   platos: [{ name: 'fish', image: 'assets/images/mediterranean food/fish.png' }, { name: 'Frittata', image: 'assets/images/mediterranean food/frittata.png' }, { name: 'Lasagna', image: 'assets/images/mediterranean food/lasagna.png' }, { name: 'Panna-Cotta', image: 'assets/images/mediterranean food/panna-cotta.png' }, { name: 'Risotto', image: 'assets/images/mediterranean food/risotto.png' }]
@@ -21,7 +24,9 @@ var data = [{
   name: 'Mega Hamburguesa',
   categoria: 'americana',
   descripcion: 'lorem ipsum',
-  addres: 'Av. Jacinto Lara',
+  Precio: '1.000.000 Pesos',
+
+  address: 'Av. Jacinto Lara',
   tlf: '737328239832',
   coords: { lat: 12.1847948, lng: -70.0205299 },
   platos: [{ name: 'BBQ', image: 'assets/images/american food/bbq.png' }, { name: 'Cake', image: 'assets/images/american food/cake.png' }, { name: 'Fast-Food', image: 'assets/images/american food/fast-food.png' }, { name: 'French-Fries', image: 'assets/images/american food/french-fries.png' }, { name: 'Hot Dogs', image: 'assets/images/american food/hot-dogs.png' }]
@@ -31,7 +36,9 @@ var data = [{
   name: 'La Media Naranja',
   categoria: 'italiana',
   descripcion: 'lorem ipsum',
-  addres: 'Av. Jacinto Lara',
+  Precio: '1.000.000 Pesos',
+
+  address: 'Av. Jacinto Lara',
   tlf: '737328239832',
   coords: { lat: 12.1201766, lng: -69.9099454 },
   platos: [{ name: 'Pasta', image: 'assets/images/italian food/pasta.png' }, { name: 'Pizza', image: 'assets/images/italian food/pizza.png' }, { name: 'Ravioli', image: 'assets/images/italian food/ravioli.png' }, { name: 'Spaghetti', image: 'assets/images/italian food/spaghetti.png' }, { name: 'Croissant', image: 'assets/images/italian food/croissant.png' }]
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,20 @@ $(document).ready(function () {
 
 });
 var mapa;
+var mapa2;
 var opciones = {
   center: { lat: 11.9108084, lng: -70.0299834 },
-  zoom: 10
+  zoom: 9
 };
+var restaurantes = [];
 
 
 
 function initMap() {
   mapa = new google.maps.Map(document.getElementById('map'), opciones);
+  mapa2 = new google.maps.Map(document.getElementById('modalbody'), opciones);
 
-  var restaurantes = [];
+
   for (let i = 0; i < data.length; i++) {
     const restaurante = data[i];
     var marker = new google.maps.Marker({
@@ -21,13 +24,54 @@ function initMap() {
       map: mapa,
       icon: {
         path: google.maps.SymbolPath.CIRCLE,
-        scale: 10,
+        scale: 8,
         fillColor: 'magenta',
         fillOpacity: 0.8,
-      }
+      },
+      description: restaurante.descripcion,
+      address: restaurante.address,
+      tlf: restaurante.tlf,
+      precio: restaurante.Precio,
     });
     marker.setMap(mapa);
     restaurantes.push(marker);
+
+    marker.addListener('click', function () {
+      $('#myModal').find('.modal-title').text(this.title);
+      $('#myModal').find('.modal-desc').text(this.description);
+      $('#myModal').find('.modal-tlf').text(this.tlf);
+      $('#myModal').find('.modal-precio').text(this.precio);
+
+
+
+
+      //google.maps.event.trigger(mapa2, 'resize');
+
+      this.setMap(mapa2);
+      mapa2.setZoom(15);
+      mapa2.setCenter(this.position);
+
+      //map.setMap($('#myModal').find('.modal-body'));
+
+
+
+
+
+
+
+
+
+      $('#myModal').modal('show');
+
+      $('#myModal').on('shown.bs.modal', function () {
+        var lastCenter = mapa2.getCenter();
+        google.maps.event.trigger(mapa2, 'resize');
+        mapa2.setCenter(lastCenter);
+
+      });
+
+
+    });
 
   }
 
@@ -53,24 +97,19 @@ function setMarkers() {
 //   //markers = [];//use this to clear out all the markers.
 // }
 
-$(document).ready(function () {
 
-});
-var mapa;
-var opciones = {
-  center: { lat: 11.9108084, lng: -70.0299834 },
-  zoom: 10
-};
+
 
 
 $(document).ready(function () {
   $('.fa-search').click(function () {
     var categoria = $('#filtro-categoria').val();
     for (var j = 0; j < data.length; j++) {
-      if (categoria === data[j].categoria) {
+      const restaurante = data[j];
+      if (restaurante.categoria.includes(categoria)) {
         var position = j;
         mapa = new google.maps.Map(document.getElementById('map'), opciones);
-        const restaurante = data[j];
+
         var marker = new google.maps.Marker({
           position: new google.maps.LatLng(data[j].coords),
           title: restaurante.categoria,
@@ -84,18 +123,25 @@ $(document).ready(function () {
         });
       }
     }
+
+    var boxMenu = $('.menu');
+    boxMenu.html('');
     for (var k = 0; k < data[position].platos.length; k++) {
-      var boxMenu = $('.menu');
+
       boxMenu.append('<img src ="' + data[position].platos[k].image + '"/>');
+
     }
   });
-});
 
-$(document).ready(function () {
-  $('.linea').click(function () {
+  $('.modal-close').click(function () {
+    initMap();
 
   });
 });
+
+
+
+
 
 
 
